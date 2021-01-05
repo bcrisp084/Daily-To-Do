6a5d21310869c52a99af90e61c46f1cb932e4556
Brian crisp <bcrisp084@yahoo.com>
2021-01-05 18:05:37 -0500

    added function to create color blocks

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,53 @@
-var inputNine = $("#infoNineAm");
-var inputTen = $("#infoTenAm");
-var inputEleven = $("#infoElevenAm");
-var inputTwelve = $("#infoTwelvePm");
-var inputOne = $("#infoOnePm");
-var inputTwo = $("#infoTwoPm");
-var inputThree = $("#infoThreePm");
-var inputFour = $("#infoFourPm");
-var inputFive = $("#infoFivePm");
+var inputNine = $("#09");
+var inputTen = $("#10");
+var inputEleven = $("#11");
+var inputTwelve = $("#12");
+var inputOne = $("#13");
+var inputTwo = $("#14");
+var inputThree = $("#15");
+var inputFour = $("#16");
+var inputFive = $("#17");
 
 $(document).ready(function () {
-  var curretnDate = moment().format("DD MMM YYYY, h:mm:ss a");
+  var curretnDate = moment().format("MMMM Do YYYY, h:mm:ss a");
   $("#currentDate").text(curretnDate);
   var currentDay = moment().format("dddd");
   $("#currentDay").text(currentDay);
 });
 
+function displayColor() {
+  let currentHour = moment().format("HH");
+  $(".form-control").each(function () {
+    let coloredBlock = $(this).attr("id");
+    if (currentHour > coloredBlock) {
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+
+    if (currentHour === coloredBlock) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    }
+
+    if (currentHour < coloredBlock) {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+}
+
+displayColor();
+
+setInterval(function () {
+  displayColor();
+}, 1000 * 60);
+
 $(".saveBtn").click(function () {
   var userInputNine = inputNine.val();
   localStorage.setItem("userInputNine", userInputNine);
+  console.log("userInputNine:", userInputNine);
   var userInputTen = inputTen.val();
   localStorage.setItem("userInputTen", userInputTen);
   var userInputEleven = inputEleven.val();
@@ -56,4 +86,5 @@ function displayUserInput() {
   var savedInputFive = localStorage.getItem("userInputFive");
   inputFive.text(savedInputFive);
 }
+
 displayUserInput();
